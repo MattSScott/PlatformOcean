8f544a4492714f39bc75ea8d9fbffe645e060011
Matthew Scott <matthew.scott3142@gmail.com>
2022-11-17 00:17:06 +0000

    text changes on hover (fix required)

--- meta-platform/src/Components/Network/Network.js
+++ meta-platform/src/Components/Network/Network.js
@@ -1,6 +1,7 @@
 import * as Utils from "./NetworkUtils.js";
 import { useSprings, animated } from "react-spring";
 import { useGesture, useHover, useDrag } from "@use-gesture/react";
+import { useState } from "react";
 import "./Network.css";
 
 const boxWidth = 200;
@@ -15,16 +16,33 @@ const nodeUpdater =
   };
 
 export default function Network() {
-  const dataset = Utils.genItems(boxWidth, boxHeight);
+  const [dataset] = useState(Utils.genItems(boxWidth, boxHeight));
+  const texts = Utils.itemDescriptors;
+  const altTexts = Utils.itemInfos;
 
   const [springs, setSprings] = useSprings(dataset.length, (item) => ({
     config: { duration: 500 },
     ...dataset[item],
   }));
 
+  const [textSprings, setTextSprings] = useState(
+    new Array(texts.length).fill(false)
+  );
+
   const bind = useHover(({ args: [index], active }) => {
     setSprings.start(nodeUpdater(active, index, dataset));
+    setTextSprings(textSprings.map((v, i) => (i === index ? !v : v)));
   });
+
+  //   const fadeText = useTransition(textSprings, {
+  //     from: { position: "absolute", opacity: 0 },
+  //     enter: { opacity: 1 },
+  //     leave: { opacity: 0 },
+  //     reverse: toggle,
+  //     delay: 200,
+  //     config: config.molasses,
+  //     onRest: () => set(!toggle),
+  //   });
 
   return (
     <svg width="100%" height="100%" viewBox={`0 0 ${boxWidth} ${boxHeight}`}>
@@ -39,17 +57,30 @@ export default function Network() {
             stroke="black"
           />
           <animated.circle {...bind(i)} key={`circle_${i}`} style={styles} />
-          <animated.text
+          {/* <animated.text
             key={`text_${i}`}
             x={styles.cx}
             y={styles.cy}
+            // opacity={textSprings[i].opacity}
             fontSize="5px"
             textAnchor="middle"
             fill="white"
             dy=".3em"
           >
-            {Utils.itemDescriptors[i]}
-          </animated.text>
+            {textSprings[i] ? altTexts[i] : texts[i]}
+          </animated.text> 
+          */}
+          <animated.foreignObject
+            {...bind(i)}
+            x={styles.cx.get() - styles.r.get()}
+            y={styles.cy.get() - styles.r.get()}
+            width={styles.r.get() * 2}
+            height={styles.r.get() * 2}
+          >
+            <div className="circText" color="black">
+              {textSprings[i] ? altTexts[i] : texts[i]}
+            </div>
+          </animated.foreignObject>
         </>
       ))}
       <circle
--- meta-platform/src/Components/Network/NetworkUtils.js
+++ meta-platform/src/Components/Network/NetworkUtils.js
@@ -15,6 +15,17 @@ export const itemDescriptors = [
   "Ecosystem",
 ];
 
+export const itemInfos = [
+  "Each group or conversation in the client is with a different server, but open standard transfer protocols and programming interfaces provide seamless client-side integration, protect data, preserves privacy and prevent data leakage",
+  "The plug-in architecture also supports codification of the deep social knowledge captured by each of the five sets of design principles",
+  "The creation of plug-ins is a reflective process, as communities of developers use an instance of the platform to develop plug-ins for other instances",
+  "A tool to support the creation of new tools that were not envisaged by the designer of the original tool",
+  "The system architecture allows a range of options for self-hosting, with multi-purpose multi-function self-configuration implemented through plug-ins, supporting decentralisation",
+  "For collective action, feedback to individuals and communities and how their small actions X contributed to a greater action Y which had significant impact Z",
+  "The generic platform can be used to create new downloadable instances of the platform, which others can use for faster customisation for related applications (we refer to this process as ‘parkrunification’ after the explosion of popularity of the parkrun phenomenon following the same process of observation and imitation)",
+  "The creation of a platform ecosystem supports sustainability through diversity and inhibits monopoly",
+];
+
 const getRad = (a, b, theta) => {
   let xPol = b * Math.cos(theta);
   let yPol = a * Math.sin(theta);
@@ -24,17 +35,15 @@ const getRad = (a, b, theta) => {
 };
 
 const getRandRange = () => {
-    let sclFac = randULim - randLLim;
-    return Math.random() * sclFac + randLLim;
-}
+  let sclFac = randULim - randLLim;
+  return Math.random() * sclFac + randLLim;
+};
 
 export const genItems = (xoff, yoff) => {
-  //   let rad = Math.min(xoff, yoff) / 2;
   let circInc = (2 * Math.PI) / numItems;
   let items = [];
   let index = 0;
   for (let i = 0; i < 2 * Math.PI; i += circInc) {
-    
     let rad = getRad(xWidth, yHeight, i);
     let randRange = getRandRange();
     let dist = randRange * rad;
@@ -44,9 +53,8 @@ export const genItems = (xoff, yoff) => {
       cx: dist * Math.cos(i) + xoff / 2,
       cy: dist * Math.sin(i) + yoff / 2,
       fill: colourPalette[randCol],
-      r: randSize,
+      r: Math.sqrt(itemInfos[index].length) * 1.5,
       idx: index,
-    //   toggled: false,
     });
     index++;
   }
