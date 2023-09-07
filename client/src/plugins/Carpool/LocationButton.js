8b1f383817d6b3389647a06fa5902ff8d7a534b5
Matt Scott <matthew.scott3142@gmail.com>
2023-09-07 14:31:40 +0100

    add regex checker to locationButton

--- client/src/plugins/Carpool/LocationButton.js
+++ client/src/plugins/Carpool/LocationButton.js
@@ -25,6 +25,16 @@ export default function LocationButton({
   };
 
   const handleSave = () => {
+    const ValidPostcodeRegex =
+      /([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/g;
+
+    const PostcodeIsValid = ValidPostcodeRegex.test(postcode);
+
+    if (!PostcodeIsValid) {
+      alert("Postcode format is invalid.");
+      return;
+    }
+
     const NewLocation = {
       name: name,
       postcode: postcode,
