fdec9088b6a1f41915cec5abe27fe4a123a6ac43
Matt Scott <matthew.scott3142@gmail.com>
2024-12-02 19:54:36 +0000

    add suspense to remote plugin

--- client/src/ImportUtils/RemotePluginPipeline.js
+++ client/src/ImportUtils/RemotePluginPipeline.js
@@ -4,6 +4,7 @@ import { useClientDataContext } from "../Contexts/ClientContext";
 import { ConsultPluginCache } from "../remoteImporterUtils/pluginCache";
 import PluginWrapper from "../PluginWrapper/Wrapper";
 import ErrorBoundary from "../remoteImporterUtils/errorBoundary";
+import FetchingPlugin from "../PluginWrapper/FetchingPlugin";
 
 export default function RemotePluginPipeline({
   remoteUrl,
@@ -34,17 +35,19 @@ export default function RemotePluginPipeline({
         style={{ width: "100%", height: "100%" }}
         title={`iframe-${pluginKey}`}
       >
-        {mountNode &&
-          RemoteComponent &&
-          createPortal(
-            <DistributedRemoteComponent
-              {...props}
-              routingKey={pluginKey}
-              client={client}
-              uniqueClientID={clientID}
-            />,
-            mountNode
-          )}
+        <React.Suspense fallback={<FetchingPlugin />}>
+          {mountNode &&
+            RemoteComponent &&
+            createPortal(
+              <DistributedRemoteComponent
+                {...props}
+                routingKey={pluginKey}
+                client={client}
+                uniqueClientID={clientID}
+              />,
+              mountNode
+            )}
+        </React.Suspense>
       </iframe>
     </ErrorBoundary>
   );
