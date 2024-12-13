1088caec049ba1255de6c2c4a08fdca27db14362
Matt Scott <matthew.scott3142@gmail.com>
2024-12-13 03:07:34 +0000

    add button to return to home

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import Gateway from "./Gateway/Gateway";
 import Multicaster from "./ServiceDiscovery/Multicaster";
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
 function App() {
   const [isDiscovering, setIsDiscovering] = useState(true);
@@ -11,6 +11,11 @@ function App() {
   const bindEndpoint = (chosenEndpoint) => {
     setIsDiscovering(false);
     setEndpoint(chosenEndpoint);
+  };
+
+  const unbindEndpoint = () => {
+    setIsDiscovering(true);
+    setEndpoint(null);
   };
 
   return (
@@ -45,7 +50,16 @@ function App() {
         {isDiscovering ? (
           <Multicaster bindEndpoint={bindEndpoint} />
         ) : (
-          <Gateway endpoint={endpoint} />
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              variant="contained"
+              style={{ position: "absolute", top: 0 }}
+              onClick={unbindEndpoint}
+            >
+              Return to Server Select
+            </Button>
+            <Gateway endpoint={endpoint} />
+          </div>
         )}
       </header>
     </div>
