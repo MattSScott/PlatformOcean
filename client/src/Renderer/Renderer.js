d77f500c2076a00089e9725fb7abcb8575547a8b
Matt Scott <matthew.scott3142@gmail.com>
2025-01-09 18:40:36 +0000

    simplify renderer

--- client/src/Renderer/Renderer.js
+++ client/src/Renderer/Renderer.js
@@ -1,24 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import PluginAdder from "../PluginAdder/PluginAdder";
 import "./Renderer.css";
 import { ClientDataContext } from "../Contexts/ClientContext";
+import { NetworkIPContext } from "../Contexts/ServerIPContext";
 
-class Renderer extends React.Component {
-  render() {
-    return (
-      <div className="renderer">
-        <div className="logout">
-          <PluginAdder />
-          <ClientDataContext.Consumer>
-            {({ clientID }) => (
-              <p>Signed in as: {`${clientID.substring(0, 8)}...`}</p>
-            )}
-          </ClientDataContext.Consumer>
-        </div>
-        <div className="allComps">{this.props.loadedPlugins}</div>
+export default function Renderer({ loadedPlugins }) {
+  const clientContext = useContext(ClientDataContext);
+  const networkAddress = useContext(NetworkIPContext);
+
+  return (
+    <div className="renderer">
+      <div className="logout">
+        <PluginAdder networkAddress={networkAddress} />
+        <p>Signed in as: {`${clientContext.clientID.substring(0, 8)}...`}</p>
       </div>
-    );
-  }
+      <div className="allComps">{loadedPlugins}</div>
+    </div>
+  );
 }
-
-export default Renderer;
