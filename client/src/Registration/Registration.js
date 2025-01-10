eb8d5ba6f0c595c0b62c907eed1bd136b80681ee
Matt Scott <matthew.scott3142@gmail.com>
2025-01-10 01:36:02 +0000

    remove unused registration code

--- client/src/Registration/Registration.js
+++ client/src/Registration/Registration.js
@@ -3,149 +3,11 @@ import {
   TextField,
   Button,
   Typography,
-  // Box,
   IconButton,
   InputAdornment,
 } from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import "./Registration.css";
-
-// class Registration extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.setUsername = this.setUsername.bind(this);
-//     this.setPassword = this.setPassword.bind(this);
-//     this.submitSignup = this.submitSignup.bind(this);
-//     this.submitLogin = this.submitLogin.bind(this);
-//     this.loadUsers = this.loadUsers.bind(this);
-//   }
-
-//   static contextType = NetworkIPContext;
-
-//   state = {
-//     users: [],
-//     formEntry: { username: null, password: null },
-//   };
-
-//   componentDidMount() {
-//     this.loadUsers();
-//     this.tryFindClient();
-//   }
-
-//   tryFindClient() {
-//     const loggedInUser = localStorage.getItem("userID");
-//     if (loggedInUser) {
-//       this.props.setClientInfo(loggedInUser);
-//     }
-//   }
-
-//   setUsername(event) {
-//     this.setState((prevState) => ({
-//       ...prevState,
-//       formEntry: { ...prevState.formEntry, username: event.target.value },
-//     }));
-//   }
-
-//   setPassword(event) {
-//     this.setState((prevState) => ({
-//       ...prevState,
-//       formEntry: { ...prevState.formEntry, password: event.target.value },
-//     }));
-//   }
-
-//   async loadUsers() {
-//     const UserListAddress = `${this.context}/registry/getAll`;
-//     try {
-//       const UserData = await fetch(UserListAddress);
-//       const UserJSON = await UserData.json();
-//       this.setState((prevState) => ({ ...prevState, users: UserJSON }));
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-
-//   async submitSignup(e) {
-//     e.preventDefault();
-
-//     try {
-//       const numUsers = Object.keys(this.state.users).length;
-//       let registrationAddress = `${this.context}/registry/`;
-//       if (numUsers === 0) {
-//         registrationAddress += "own";
-//       } else {
-//         registrationAddress += "add";
-//       }
-//       const response = await fetch(registrationAddress, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(this.state.formEntry),
-//       });
-//       const UUID_Resp = await response.json();
-
-//       this.props.setClientInfo(UUID_Resp);
-//     } catch (error) {
-//       console.log(error);
-//       alert("Duplicate username entered.");
-//     }
-//   }
-
-//   async submitLogin(e) {
-//     e.preventDefault();
-//     try {
-//       const RegistrationAddress = `${this.context}/registry/get`;
-//       const response = await fetch(RegistrationAddress, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(this.state.formEntry),
-//       });
-
-//       const UUID_Resp = await response.json();
-
-//       this.props.setClientInfo(UUID_Resp);
-//     } catch (error) {
-//       console.log(error);
-//       alert("Username or password is incorrect.");
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <div className="inputHouse">
-//           <TextField
-//             className="inputButton"
-//             id="outlined-basic"
-//             label="Username"
-//             variant="outlined"
-//             onChange={this.setUsername}
-//           />
-//           <TextField
-//             className="inputButton"
-//             id="outlined-basic"
-//             label="Password"
-//             variant="outlined"
-//             onChange={this.setPassword}
-//           />
-//         </div>
-//         <div className="buttonHouse">
-//           <Button variant="contained" onClick={this.submitSignup}>
-//             SignUp
-//           </Button>
-//           <Button variant="contained" onClick={this.submitLogin}>
-//             Login
-//           </Button>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Registration;
 
 export default function Registration({ setUserDetails }) {
   const [username, setUsername] = useState("");
@@ -199,6 +61,9 @@ export default function Registration({ setUserDetails }) {
         }}
       >
         <TextField
+          type="text"
+          autoComplete="off"
+          autoCapitalize="none"
           sx={{
             marginBottom: "10px",
             "& .MuiInputBase-root": {
@@ -217,7 +82,7 @@ export default function Registration({ setUserDetails }) {
         <TextField
           type={showPassword ? "text" : "password"}
           autoComplete="off"
-          autoCapitalize="off"
+          autoCapitalize="none"
           sx={{
             marginTop: "10px",
             "& .MuiInputBase-root": {
@@ -251,7 +116,6 @@ export default function Registration({ setUserDetails }) {
       <div
         style={{
           display: "flex",
-          // marginTop: "10px",
           alignContent: "center",
           alignItems: "center",
           justifyContent: "center",
