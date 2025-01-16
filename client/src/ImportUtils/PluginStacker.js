03de5b7358f5cae431d263b34bbe585da26b5156
Matt Scott <matthew.scott3142@gmail.com>
2025-01-16 17:50:26 +0000

    convert `PluginStacker` to functional component

--- client/src/ImportUtils/PluginStacker.js
+++ client/src/ImportUtils/PluginStacker.js
@@ -1,86 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { Button } from "@mui/material";
 import "./pluginStyle.css";
 
-export default class PluginStacker extends React.Component {
-  constructor() {
-    super();
-    this.cycleLeft = this.cycleLeft.bind(this);
-    this.cycleRight = this.cycleRight.bind(this);
-  }
+export default function PluginStacker({ plugins }) {
+  const [currentIdx, setCurrentIdx] = useState(0);
+  const cycleLength = plugins.length;
 
-  state = {
-    currentIdx: 0,
-  };
-
-  recalculateIdx(idx, change, cycleLength) {
-    let newIdx = idx + change;
+  const recalculateIdx = (idx, change) => {
+    const newIdx = idx + change;
     if (newIdx < 0) {
       newIdx += cycleLength;
     }
     return newIdx % cycleLength;
-  }
+  };
 
-  cycleLeft() {
-    const CycleLength = this.props.plugins.length;
-    this.setState((prevState) => ({
-      ...prevState,
-      currentIdx: this.recalculateIdx(prevState.currentIdx, -1, CycleLength),
-    }));
-  }
+  const cycleLeft = () => {
+    setCurrentIdx((prev) => recalculateIdx(prev, -1));
+  };
 
-  cycleRight() {
-    const CycleLength = this.props.plugins.length;
-    this.setState((prevState) => ({
-      ...prevState,
-      currentIdx: this.recalculateIdx(prevState.currentIdx, 1, CycleLength),
-    }));
-  }
+  const cycleRight = () => {
+    setCurrentIdx((prev) => recalculateIdx(prev, 1));
+  };
 
-  render() {
-    const PluginArray = this.props.plugins.map((Plugin, idx) => (
-      <div
-        key={`stack_box_${idx}`}
-        className="componentHouse"
-        style={{
-          display: idx === this.state.currentIdx ? "" : "none",
-        }}
-      >
-        {Plugin}
-      </div>
-    ));
+  const PluginArray = plugins.map((Plugin, idx) => (
+    <div
+      key={`stack_box_${idx}`}
+      className="componentHouse"
+      style={{
+        display: idx === currentIdx ? "" : "none",
+      }}
+    >
+      {Plugin}
+    </div>
+  ));
 
-    return (
-      <div>
-        {PluginArray}
-        {this.props.plugins.length > 1 && (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
+  return (
+    <div>
+      {PluginArray}
+      {cycleLength > 1 && (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Button
+            variant="outlined"
+            onClick={cycleLeft}
+            style={{ marginRight: "5px" }}
           >
-            <Button
-              variant="outlined"
-              onClick={this.cycleLeft}
-              style={{ marginRight: "5px" }}
-            >
-              <ArrowBackIcon />
-            </Button>
-            <Button
-              variant="outlined"
-              onClick={this.cycleRight}
-              style={{ marginLeft: "5px" }}
-            >
-              <ArrowForwardIcon />
-            </Button>
-          </div>
-        )}
-      </div>
-    );
-  }
+            <ArrowBackIcon />
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={cycleRight}
+            style={{ marginLeft: "5px" }}
+          >
+            <ArrowForwardIcon />
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 }
